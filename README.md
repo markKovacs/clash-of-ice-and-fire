39964f92d1e0ad1a0846725dfe12c388f1c7c38f
Mark Kovacs <kovacsmark89@gmail.com>
2019-01-06 18:13:49 +0100

    Separated frontend and backend into folders

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Clash of Ice and Fire application
+Clash of Ice and Fire application!
---
+++ frontend/src/app/containers/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  styleUrls: ['app.component.scss'],
+  template: `
+    <div>
+      Clash of Ice and Fire
+    </div>
+  `
+})
+export class AppComponent {
+  constructor() { }
+}
