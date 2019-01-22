bdd76f7ae20b2f5eb24a7698d878e68871e01e65
Mark Kovacs <kovacsmark89@gmail.com>
2019-01-22 16:59:36 +0100

    Remove Jdbc resource server implementation from config and rename it

--- api/src/main/java/com/coinf/config/OAuth2ResourceServerConfigJdbc.java
+++ api/src/main/java/com/coinf/config/OAuth2ResourceServerConfigJdbc.java
@@ -1,27 +1,24 @@
 package com.coinf.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
-@Configuration
-@PropertySource({ "classpath:persistence.properties" })
-@EnableResourceServer
-public class OAuth2ResourceServerConfig extends ResourceServerConfigurerAdapter {
+import javax.sql.DataSource;
+
+//@Configuration
+//@PropertySource({ "classpath:persistence.properties" })
+//@EnableResourceServer
+public class OAuth2ResourceServerConfigJdbc extends ResourceServerConfigurerAdapter {
 
     @Autowired
     private Environment env;
