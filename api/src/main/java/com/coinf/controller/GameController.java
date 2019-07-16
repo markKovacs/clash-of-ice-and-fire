17ca46deb882f325032d446550b9cb7de85ec459
Mark Kovacs <kovacsmark89@gmail.com>
2019-07-16 11:06:01 +0200

    Added faction info and deleted unnecessary controller

--- api/src/main/java/com/coinf/controller/GameController.java
+++ api/src/main/java/com/coinf/controller/GameController.java
@@ -11,6 +11,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GameController {
 
+    // TODO: This should return a GameDto, which is the current state of whole game,
+    //  containing the whole board with unit and all info, and also other game info like player and faction mats.
+    //  Could be separated into several services... use builder pattern for dto object constructions.
+
     @Autowired
     private AuthExtractionUtil authExtractUtil;
 
@@ -22,7 +26,8 @@ public class GameController {
         String authName = authExtractUtil.getAuthName(auth);
         // TODO: Look up user based on authName, then you also get if it has a current active game or not,
         //  or even if he registered with an in-game username
-        //  Important: we look up the Account and Game objects but we need to have a DTO generator on top of it
+        //  Important: we look up the Account and Game objects
+        //  but we need to have a DTO generator on top of it to return a flattened object to frontend
         return gameService.getGame(authName);
     }
 
